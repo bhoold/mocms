9e109da6a7ac7d135b7a6fc7cca4922a1b0403c8
Renven <bhoold@qq.com>
2017-06-03 21:02:10 +0800

    修改配置

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'app';
+	$application_folder = 'admin';
 
 /*
  *---------------------------------------------------------------
